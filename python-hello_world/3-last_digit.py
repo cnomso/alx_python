f571178db4900bf0aa42b5a036d425bcfc0efadb
cnomso <cnomso@gmail.com>
2023-07-23 15:34:54 +0200

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -11,11 +11,4 @@ if last_digi < 6 and last_digi !=0:
     print('The Last digit of {0} is {1} and is less than 6 and not 0' .format(number, last_digi))
 
 if last_digi == 0: 
-    print('The Last digit of {0} is {1} and is greater than 5' .format(number, last_digi))    
-
-
-# result = "and is greater than 5" if last_digi > 5  else ("and is less than 6 and not 0" if last_digi < 6 && !=0  else "and is 0" if last_digi == 0 )
-
-
-
-# print('The Last digit of {0} is {1} {2}' .format(number, last_digi, result))
+    print('The Last digit of {0} is {1} and is greater than 5' .format(number, last_digi))
