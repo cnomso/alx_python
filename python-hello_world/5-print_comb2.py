5856884d1b64f1a6c016327594bd2685f7a0ff18
cnomso <cnomso@gmail.com>
2023-07-26 00:32:05 +0200

    print 0 -99 :02d

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,10 +1,5 @@
 # Program that prints numbers from 0 to 99
 
-# number = 0
-# while number <= 99: 
-#     print("{:02d}, " .format(number))
-#     number +=1
-
 for number in range(100):
     if number != 99:
         print(number, end=", ")
