704c32feed852c0b009970a39895a966a5277f32
cnomso <cnomso@gmail.com>
2023-07-26 00:25:41 +0200

    print 0 -99 :02d

---
+++ python-hello_world/5-print_comb2.py
@@ -0,0 +1,9 @@
+# Program that prints numbers from 0 to 99
+
+# for num in range(100):
+#     print("{}, " .format(num))
+
+number = 0
+while number <= 99: 
+    print("{:02d}, " .format(number))
+    number +=1
