e32ed14a3d9cec8e170204b54c079439c83667aa
cnomso <cnomso@gmail.com>
2023-07-26 20:44:27 +0200

    Fibonacci seq

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,14 @@
+# Write a Python function called fibonacci_sequence that takes a number n as input and returns a list of the first n Fibonacci numbers.
+
+def fibonacci_sequence(n):
+    
+    if n <= 0:
+        return []
+    
+    sequence = [0,1]
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+
+    return sequence
+
