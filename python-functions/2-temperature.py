f8bbcf6ca8b01bb9643a4283f4a84e3874973938
cnomso <cnomso@gmail.com>
2023-07-26 13:29:54 +0200

    Temperature convertor

---
+++ python-functions/2-temperature.py
@@ -0,0 +1,5 @@
+# Write a Python function called convert_to_celsius that takes a temperature in Fahrenheit as input and returns the temperature in Celsius.
+
+def convert_to_celsius(fahrenheit):
+    result = (fahrenheit - 32) * 5 / 9
+    return result
