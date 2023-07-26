158fb89164214716f8fa75023b0c666496bad6ec
cnomso <cnomso@gmail.com>
2023-07-26 20:46:46 +0200

    Fibonacci seq

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -5,7 +5,7 @@ def fibonacci_sequence(n):
     if n <= 0:
         return []
     
-    sequence = [0]
+    sequence = [0,1]
     while len(sequence) < n:
         next_number = sequence[-1] + sequence[-2]
         sequence.append(next_number)
