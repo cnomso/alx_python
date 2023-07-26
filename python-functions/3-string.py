6b9e07aa88c639ec7268f9b9feb9bf7b2c8b542b
cnomso <cnomso@gmail.com>
2023-07-26 13:36:02 +0200

    reverse string

---
+++ python-functions/3-string.py
@@ -0,0 +1,5 @@
+# Write a Python function called reverse_string that takes a string as input and returns the reverse of that string. 
+
+def reverse_string(string): 
+    rstring = string[::-1]
+    return rstring
