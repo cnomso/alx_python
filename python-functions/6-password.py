a68dd76b47bf7f00b2bc383e7d7b28ad5f30c547
cnomso <cnomso@gmail.com>
2023-07-26 21:21:02 +0200

    password verify

---
+++ python-functions/6-password.py
@@ -0,0 +1,17 @@
+# Write a Python function called validate_password that takes a password as input and performs the following checks:
+
+def validate_password(password):
+    if len(password) < 8:
+        return False
+
+    if not any(char.isupper() for char in password):
+        return False
+
+    if not any(char.islower() for char in password):
+        return False
+
+    if not any(char.isdigit() for char in password):
+        return False
+
+
+    return True
