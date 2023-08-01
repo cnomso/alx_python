346360445cda791eebfbf80a06c156c211c7045f
cnomso <cnomso@gmail.com>
2023-08-01 22:53:25 +0200

    import function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 from add_0 import add
+def main():
+
+
+    a = 1
+    b = 2
+    result = add(1,2)
+    print("{} + {} = {}".format(a,b,result))
 
 if __name__ == "__main__":
-
-a = 1
-b = 2
-result = add(1,2)
-print("{} + {} = {}".format(a,b,result))
+    main() 
