c73b08f71195288a9b489c34f617e17ddc10c8b6
cnomso <cnomso@gmail.com>
2023-08-01 22:34:12 +0200

    argv arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -26,10 +26,12 @@ import sys
 def main():
     arguments = sys.argv[1:]
     num_arguments = len(arguments)
-    if num_arguments < 2:
-        print("{} argument:" .format(num_arguments), end="")
-    else:
+    if num_arguments > 1:
         print("{} arguments:" .format(num_arguments), end="")
+    elif num_arguments == 0:
+         print("{} arguments:" .format(num_arguments), end="")    
+    else:
+        print("{} argument:" .format(num_arguments), end="")
     if num_arguments == 0:
         print(".")
     else:
