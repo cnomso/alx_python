5d9337973c543ed5696a1b09d2985256be7db8f1
cnomso <cnomso@gmail.com>
2023-08-02 23:52:19 +0200

    Present C

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,14 @@
+#Write a function that returns a set of common elements in two sets.
+
+def common_elements(set_1, set_2):
+    common_elements_sets = set_1.intersection(set_2)
+    return common_elements_sets
+  
+    
+    
+    #  noC = ""
+    # for char in my_string:
+    #     if char != 'c' and char != 'C':
+    #         noC += char
+
+    # return noC
