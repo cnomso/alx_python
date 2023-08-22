db4f5f733e50ffd73b969579fecf290f03946a59
cnomso <cnomso@gmail.com>
2023-08-22 21:17:14 +0200

    square size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+''' class of square
+    creates private size
+'''
+class Square():
+    ''' instantiate size
+    private attribute: size
+    '''
+    def __init__(self, size):
+        self._Square__size = size 
