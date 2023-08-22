a2d8f2b8cc7d2a82ee91489303d37d818cbcac4b
cnomso <cnomso@gmail.com>
2023-08-22 22:17:45 +0200

    square size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,14 +5,26 @@ class Square():
     ''' instantiate size
     private attribute: size
     '''
-    def __init__(self, size= 0):
+    def __init__(self, size=0):
         self._Square__size = size 
         
-
-    def __dict__(self, size):
-        self._Square__size = size 
-        if type(self.size) is not int:
+        if type(size) is not int:
             raise TypeError('size must be an integer')
-        elif self.size < 0:
+            
+        elif size < 0:
             raise ValueError('size must be >= 0')
             
+        else:
+            pass
+
+
+    # def __dict__(self, size= 0):
+    #     self._Square__size = size 
+    #     if type(self.size) is not int:
+    #         # raise TypeError('size must be an integer')
+    #         print('size must be an integer')
+    #     elif self.size < 0:
+    #         # raise ValueError('size must be >= 0')
+    #         print('size must be >= 0')
+    #     else:
+    #         pass
