f5aa2fc92f8cc0d3eb480a4c41b6009a29cc2ff5
cnomso <cnomso@gmail.com>
2023-08-23 23:26:36 +0200

    isinstance

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,6 +1,8 @@
-'''' blah blah 
-blah
+'''' function returns boolean
 '''
 
 def is_same_class(obj, a_class):
+    '''' instantiate object and class 
+    function does this
+    '''
     return isinstance(obj, a_class)
