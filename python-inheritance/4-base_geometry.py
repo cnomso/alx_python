2d9bcb42fbe903e2d9199ed0a32e358df424615a
cnomso <cnomso@gmail.com>
2023-08-24 20:41:23 +0200

    BaseGeo improved

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,13 @@
+'''' BaseGeometry class
+'''
+
+class BaseGeometry:
+    '''' Geometry class
+    '''
+    
+    def __init__(self):
+        pass
+
+    def area(self):
+        return ('area() is not implemented')
+    
