fb7330d5d39fa074ba745f94288caf7cd4f575b0
cnomso <cnomso@gmail.com>
2023-08-24 20:26:54 +0200

    kinng of a class

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,8 @@
+'''' function returns boolean
+'''
+
+def is_kind_of_class(obj, a_class):
+    '''' instantiate object and class 
+    function does this
+    '''
+    return isinstance(obj, a_class)
