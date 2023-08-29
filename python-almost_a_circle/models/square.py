82beaa48b2c08c5214c6cbf1424f44eb1913620e
cnomso <cnomso@gmail.com>
2023-08-30 01:52:43 +0200

    square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,5 +9,17 @@ class Square(Rectangle):
 
 
 def __init__(self, size, x=0, y=0, id=None):
-    """Initialize a new Square."""
+    """Initialize a Square."""
     super().__init__(size, size, x, y, id)
+
+
+@property
+def size(self):
+    """Get/set the size of the Square."""
+    return self.width
+
+
+@size.setter
+def size(self, value):
+    self.width = value
+    self.height = value
