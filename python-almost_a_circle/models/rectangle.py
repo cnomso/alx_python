29226bd7bbe2af752b0fd1bc3d92036397a83271
cnomso <cnomso@gmail.com>
2023-08-30 00:21:36 +0200

    Rectangle 1st

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,6 @@ class Rectangle(Base):
 
     """iniatialize class Rectangle."""
 
-    # __width = width
-    # __height = height
-    # __x = x
-    # __y = y
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """call super with id"""
 
@@ -20,3 +15,44 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+        __width = width
+        __height = height
+        __x = x
+        __y = y
+
+    @property
+    def width(self):
+        """Set/get the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """Set/get the x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """Set/get the y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
