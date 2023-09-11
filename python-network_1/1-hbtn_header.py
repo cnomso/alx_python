6496b221287cf0587ad7185f0ee08fd7ba96c0da
cnomso <cnomso@gmail.com>
2023-09-11 21:34:55 +0200

    network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,3 +1,29 @@
+"""A script that takes in a URL,
+Sends a request to the URL,
+and displays the value of the variable X-Request-Id
+in the header ofthe response.
+"""
+
+import sys
 import requests
 
-response = requests.get("https://alu-intranet.hbtn.io/status")
+
+# if __name__ == "__main__":
+#     url = sys.argv[0]
+
+#     request = requests.Request(url)
+#     with requests.url(request) as resp:
+#         print(dict(resp.headers).get("X-Request-Id"))
+
+
+def get_x_request_id(url):
+    response = requests.get(url)
+    x_request_id = response.headers.get("X-Request-Id")
+    return x_request_id
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    url = sys.argv[1]
+    x_request_id = get_x_request_id(url)
