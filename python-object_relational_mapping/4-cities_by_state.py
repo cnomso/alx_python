2c45629631558eee238243288ea48fe6ebd999a1
cnomso <cnomso@gmail.com>
2023-10-07 16:53:25 +0200

    cites by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+"""
+lists all cities from the database hbtn_0e_4_usa
+
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", username=argv[1], passwd=argv[2], db=argv[3])
+    query = "SELECT cities.id, cities.name, states.name\
+             FROM cities JOIN states\
+             WHERE cities.state_id = states.id"
+    cursor = db.cursor()
+    cursor.execute(query)
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
