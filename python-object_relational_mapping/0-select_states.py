ff7625564cb8f278a200b1181a76d50d157e895b
cnomso <cnomso@gmail.com>
2023-10-07 15:28:10 +0200

    select db

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,28 +3,38 @@
 
 import MySQLdb
 import sys
+from sys import argv
 
 
-def usa_states(username, password, database):
-    db = MySQLdb.connect(
-        host="localhost", port="3306", username=username, passwrd=password, db=database
-    )
+# def usa_states(username, password, database):
+#     db = MySQLdb.connect(
+#         host="localhost", port="3306", username=username, passwrd=password, db=database
+#     )
 
-    cursor = db.cursor()
+#     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+#     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    results = cursor.fetchall()
+#     results = cursor.fetchall()
 
-    for row in results:
-        print(row)
-    cursor.close()
-    db.close()
+#     for row in results:
+#         print(row)
+#     cursor.close()
+#     db.close()
 
+
+# if __name__ == "__main__":
+#     username = sys.argv[1]
+#     password = sys.argv[2]
+#     database = sys.argv[3]
+
+#     list_states(username, password, database)
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    list_states(username, password, database)
+    db = MySQLdb.connect(host="localhost", username=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
