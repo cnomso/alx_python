dd3dbdfb012fb4781c5b3750f5da82d20780bbb4
cnomso <cnomso@gmail.com>
2023-10-07 16:54:13 +0200

    cites by state fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", username=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
     query = "SELECT cities.id, cities.name, states.name\
              FROM cities JOIN states\
              WHERE cities.state_id = states.id"
