b8941acc692e93e4c27196b8667141352c94ddc8
cnomso <cnomso@gmail.com>
2023-10-07 15:24:35 +0200

    db states select

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+"""script that lists all states from the database hbtn_0e_0_usa
+    """
+
+import MySQLdb
+import sys
+
+
+def usa_states(username, password, database):
+    db = MySQLdb.connect(
+        host="localhost", port="3306", username=username, passwrd=password, db=database
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
